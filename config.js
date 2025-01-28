0bb2fc495238bbaa29c1df8b21b87500d3d879d8
Zmylife <164553179+Zmylife@users.noreply.github.com>
2025-01-28 05:22:17 +0500

    Update config.js
    Zain khosa

--- config.js
+++ config.js
@@ -5,29 +5,29 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "",
-    CAPTION: process.env.CAPTION || "POWERED BY Arslan-MD Official",
+    SESSION_ID: process.env.SESSION_ID || "dPdjEBSQ#yHXirkwPwzRwRRz0NaTYO5sgxARgMMFCC7n67bV6RVY",
+    CAPTION: process.env.CAPTION || "_★🤍𝆺꯭𝅥SAD BOY zanii𝆺꯭𝅥🍷┼♡ ⁰³¹⁰⁴³²¹⁹⁹³",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
     AUTO_REPLY_STATUS: process.env.AUTO_REPLY_STATUS || "false",
-    READ_MESSAGE: process.env.READ_MESSAGE || "true", // Added auto-read configuration
+    READ_MESSAGE: process.env.READ_MESSAGE || "false", // Added auto-read configuration
     MODE: process.env.MODE || "public",
     AUTO_VOICE: process.env.AUTO_VOICE || "true",
-    AUTO_STICKER: process.env.AUTO_STICKER || "false",
-    AUTO_REPLY: process.env.AUTO_REPLY || "false",
+    AUTO_STICKER: process.env.AUTO_STICKER || "true",
+    AUTO_REPLY: process.env.AUTO_REPLY || "true",
     ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/71QtjpB/1737449197797.jpg",
-    ALIVE_MSG: process.env.ALIVE_MSG || "HII DEAR IM ONLINE I'M Arslan-XMD WHATSAPP BOT 😊♻️",
+    ALIVE_MSG: process.env.ALIVE_MSG || "HII DEAR IM ONLINE I'M Zanii WHATSAPP BOT 😊♻️",
     ANTI_LINK: process.env.ANTI_LINK || "true",
     ANTI_BAD: process.env.ANTI_BAD || "true",
     PREFIX: process.env.PREFIX || ".",
     FAKE_RECORDING: process.env.FAKE_RECORDING || "true",
-    FAKE_TYPING: process.env.FAKE_TYPING || "false",
+    FAKE_TYPING: process.env.FAKE_TYPING || "true",
     ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
     CURRENT_STATUS: process.env.CURRENT_STATUS || "true",
     AUTO_REACT: process.env.AUTO_REACT || "true",
     HEART_REACT: process.env.HEART_REACT || "true",
     OWNER_REACT: process.env.OWNER_REACT || "true",
     READ_CMD: process.env.READ_CMD || "true",
-    BOT_NAME: process.env.BOT_NAME || "➺ArslanƭCMD_࿐",
+    BOT_NAME: process.env.BOT_NAME || "➺zᵃⁱⁿ᭄Kʰᵒˢᵃ_࿐",
     STATUS_REPLY: process.env.STATUS_REPLY || "`➺үσυя sтαтυs sεεη נυsт ησω вү Arslan-XMD`",
     OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
 };
